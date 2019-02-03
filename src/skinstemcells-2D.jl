f8a99f79c23d5438eab9dd94a8b8343756d83d31
Marc <marc.williams.13@ucl.ac.uk>
2019-02-03 16:50:28 +0000

    Wokring 2D model

--- src/skinstemcells-2D.jl
+++ src/skinstemcells-2D.jl
@@ -42,8 +42,10 @@ struct SkinStemCellResults2D
     stemcells::Array{SkinStemCell2D, 1}
     SM::SkinStemCellModel2D
 
-    SkinStemCellResults2D(stemcells, SM) =
-    new(stemcells, SM)
+    function SkinStemCellResults2D(stemcells, SM)
+        idx = values(stemcells) .!= 0
+        return new(collect(values(stemcells))[idx], SM)
+    end
 end
 
 function copyskinstemcell(sc::SkinStemCell2D)
@@ -56,65 +58,96 @@ function createstemcellpool(SM::SkinStemCellModel2D)
     radius = 1.5 * sqrt(SM.Ns / π)
     stemcellpool = SkinStemCell2D[]
     xycoords = String[]
-    scids = DefaultDict(0, Dict((midpoint[1], midpoint[2]) => 1))
-    push!(stemcellpool,
-    SkinStemCell2D(SM.r, SM.λ, SM.Δ, SM.μd, SM.μp, Int64[], Int64[], midpoint[1], midpoint[2]))
+    stemcell = SkinStemCell2D(SM.r, SM.λ, SM.Δ, SM.μd, SM.μp, Int64[], Int64[], midpoint[1], midpoint[2])
+    stemcellpool = DefaultDict{Tuple, Any}(0)
+    push!(stemcellpool, (midpoint[1], midpoint[2]) => stemcell)
     Nstemcells = 1
     while Nstemcells < SM.Ns
         r = radius * sqrt(rand())
         θ = rand() * 2 * π
         x = round(midpoint[1] + r * cos(θ))
         y = round(midpoint[2] + r * sin(θ))
-        if scids[(x,y)] == 0
+        if stemcellpool[(x,y)] == 0
             push!(stemcellpool,
-            SkinStemCell2D(SM.r, SM.λ, SM.Δ, SM.μd, SM.μp, Int64[], Int64[], x, y))
+            (x, y) => SkinStemCell2D(SM.r, SM.λ, SM.Δ, SM.μd, SM.μp, Int64[], Int64[], x, y))
             Nstemcells += 1
-            push!(scids, (x, y) => Nstemcells)
         end
     end
-    return stemcellpool, scids
+    return stemcellpool
 end
 
-function placestemcell(scpool, randomsc, scids)
-    direction = rand([-1, 1], 2)
-    newx = scpool[randomsc].x + direction[1]
-    newy = scpool[randomsc].y + direction[2]
-    println("Current stem cell: ($(scpool[randomsc].x), $(scpool[randomsc].y), index:$(randomsc))")
-    println("New stem cell: ($(newx), $(newy))")
-    println("Size of scpool: $(length(scpool)),
-        id of new sc: $(scids[(newx,newy)])")
-    if scids[(newx,newy)] == 0
-        newstemcell = copyskinstemcell(scpool[randomsc])
-        newstemcell.x = newx
-        newstemcell.y = newy
-        push!(scpool, newstemcell)
-        push!(scids, (newx, newy) => length(scpool))
-        return scpool, scids
+function getfreespace(scpool, randomsc)
+    space = Tuple[]
+    for x in [-1, 1]
+        for y in [1, 1]
+            newx = scpool[randomsc].x + x
+            newy = scpool[randomsc].y + y
+            if scpool[(newx,newy)] == 0
+                push!(space, (newx, newy))
+            end
+        end
+    end
+
+    if isempty(space)
+        direction = rand([-1, 1], 2)
+        newx = scpool[randomsc].x + direction[1]
+        newy = scpool[randomsc].y + direction[2]
+        return (newx, newy)
     else
-        deleteat!(scpool, scids[(newx,newy)])
-        newstemcell = copyskinstemcell(scpool[randomsc])
-        newstemcell.x = newx
-        newstemcell.y = newy
-        push!(scpool, newstemcell)
-        push!(scids, (newx, newy) => length(scpool))
-        return scpool, scids
+        return rand(space)
     end
 end
 
-function cellturnover(scpool, randomsc, mutID, SM, scids; onedriver = true)
+function placestemcell(scpool::DefaultDict, randomsc)
+    (newx, newy) = getfreespace(scpool, randomsc)
+    #println("Current stem cell: ($(scpool[randomsc].x), $(scpool[randomsc].y), index:$(randomsc))")
+    #println("New stem cell: ($(newx), $(newy))")
+    #println("Size of scpool: $(length(scpool))")
+    if scpool[(newx,newy)] == 0
+        newstemcell = copyskinstemcell(scpool[randomsc])
+        newstemcell.x = newx
+        newstemcell.y = newy
+        push!(scpool, (newx, newy) => newstemcell)
+        return scpool, (newx, newy)
+    else
+        delete!(scpool, (newx, newy))
+        newstemcell = copyskinstemcell(scpool[randomsc])
+        newstemcell.x = newx
+        newstemcell.y = newy
+        push!(scpool, (newx, newy) => newstemcell)
+        return scpool, (newx, newy)
+    end
+end
+
+function cellturnover(scpool::DefaultDict, randomsc, mutID, SM; onedriver = true)
     r = rand()
+    #println("Number of cells preturnover: $(length(scpool))")
     if r < ((1 + scpool[randomsc].Δ) / 2)
-        scpool, scids = placestemcell(scpool, randomsc, scids)
-        scpool[end], mutID =
-            newmutations(scpool[end], mutID, SM, onedriver = onedriver)
+        scpool, index = placestemcell(scpool, randomsc)
+        scpool[index], mutID =
+            newmutations(scpool[index], mutID, SM, onedriver = onedriver)
     else
-        deleteat!(scpool, randomsc)
+        delete!(scpool, randomsc)
     end
-    return scpool, mutID, scids
+    #println("Number of cells posturnover: $(length(scpool))")
+    return scpool, mutID
+end
+
+function randomstemcell(scpool)
+    x = 0
+    while x == 0
+        rsc = rand(keys(scpool))
+        #println(scpool)
+        println(length(keys(scpool)))
+        if scpool[rsc] != 0
+            x = 1
+            return rsc
+        end
+    end
 end
 
 function runsimulation(SM::SkinStemCellModel2D; progress = false, restart = false, finish = "time", onedriver = true)
-    scpool, scids = createstemcellpool(SM)
+    scpool = createstemcellpool(SM)
     t = 0.0
     Rmax = SM.λ * (SM.s + 1) * SM.r * 2
     N = length(scpool)
@@ -128,22 +161,22 @@ function runsimulation(SM::SkinStemCellModel2D; progress = false, restart = fals
 
     if finish == "time"
         while t < SM.tend
-            randomsc = rand(1:length(scpool))
+            randomsc = randomstemcell(scpool)
             u1 = rand(Uniform(0, Rmax))
             if u1 < (scpool[randomsc].λ * scpool[randomsc].r * 2)
                 #println("divide")
-                scpool, mutID, scids = cellturnover(scpool, randomsc,
-                                        mutID, SM, scids,
+                scpool, mutID = cellturnover(scpool, randomsc,
+                                        mutID, SM,
                                         onedriver = onedriver)
                 Δt = (1 / (Rmax * N)) * log(1/rand())
                 t += Δt
-                N = length(scpool)
+                N = sum(values(scpool) .!= 0)
                 Ndivs += 1
             else
                 #println("die")
                 Δt = (1 / (Rmax * N)) * log(1/rand())
                 t += Δt
-                N = length(scpool)
+                N = sum(values(scpool) .!= 0)
             end
             if progress == true
                 update!(p, SM.tend - t)
@@ -151,7 +184,7 @@ function runsimulation(SM::SkinStemCellModel2D; progress = false, restart = fals
             if N == 0
                 #@warn "Population died out"
                 if restart == true
-                    scpool, scids = createstemcellpool(SM)
+                    scpool = createstemcellpool(SM)
                     t = 0.0
                     Rmax = SM.λ * (SM.s + 1) * SM.r * 2
                     N = length(scpool)
@@ -165,22 +198,22 @@ function runsimulation(SM::SkinStemCellModel2D; progress = false, restart = fals
     else
         #println("Nmax is $(Nmax)")
         while N < Nmax
-            randomsc = rand(1:length(scpool))
+            randomsc = rand(keys(scpool))
             u1 = rand(Uniform(0, Rmax))
             if u1 < (scpool[randomsc].λ * scpool[randomsc].r * 2)
                 #println("divide")
-                scpool, mutID, scids = cellturnover(scpool, randomsc,
-                                        mutID, SM, scids,
+                scpool, mutID = cellturnover(scpool, randomsc,
+                                        mutID, SM,
                                         onedriver = onedriver)
                 Δt = (1 / (Rmax * N)) * log(1/rand())
                 t += Δt
-                N = length(scpool)
+                N = sum(values(pool) .!= 0)
                 Ndivs += 1
             else
                 #println("die")
                 Δt = (1 / (Rmax * N)) * log(1/rand())
                 t += Δt
-                N = length(scpool)
+                N = sum(values(pool) .!= 0)
             end
             if progress == true
                 update!(p, SM.tend - t)
@@ -188,7 +221,7 @@ function runsimulation(SM::SkinStemCellModel2D; progress = false, restart = fals
             if N == 0
                 #@warn "Population died out"
                 if restart == true
-                    scpool, scids = createstemcellpool(SM)
+                    scpool = createstemcellpool(SM)
                     t = 0.0
                     Rmax = SM.λ * SM.r * 2
                     N = length(scpool)
--- test/test_skin2D.jl
+++ test/test_skin2D.jl
@@ -3,8 +3,10 @@ Random.seed!(1)
 Ns = 100
 SM = SkinStemCellModel2D(Ns)
 
-pool, scids = StemCellModels.createstemcellpool((SM))
+pool = StemCellModels.createstemcellpool((SM))
 # x,y coordinates of cells should all be unique
-@test length(unique(keys(scids))) == Ns
+@test length(unique(keys(pool))) == Ns
 
-runsimulation(SM)
+(newx, newy) = StemCellModels.getfreespace(pool, (50, 50))
+
+x = runsimulation(SM)
