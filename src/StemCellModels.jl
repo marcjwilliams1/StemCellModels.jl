69a5acc5f331f60caa4385995ef268eb20394002
Marc <marc.williams.13@ucl.ac.uk>
2019-08-26 20:33:49 +0100

    Import show

--- src/StemCellModels.jl
+++ src/StemCellModels.jl
@@ -6,10 +6,13 @@ using StatsBase
 using ProgressMeter
 using RecipesBase
 using DataStructures
+using UnicodePlots
 
 include("skinstemcells.jl")
 include("skinstemcells-2D.jl")
 include("plots.jl")
+
+import Base.show
 
 export
   SkinStemCellModel,
